3dcc3f73eb5648c777ae1caf70c76cea01b109b9
aparnasj-sj <61081490+aparnasj-sj@users.noreply.github.com>
2022-04-19 03:08:31 +0530

    Create Graph_dfs.cpp

---
+++ Graph_dfs.cpp
@@ -0,0 +1,47 @@
+void dfs_helper(vector<int> adj[], vector<int> &ans, map<int, bool> &visited,
+
+                                                           int src, int parent)
+
+{
+
+        visited[src] = 1;
+
+        ans.push_back(src);
+
+        for (int i : adj[src]) 
+
+        {
+
+            if (!visited[i])
+
+            {
+
+                dfs_helper(adj, ans, visited, i, src);
+
+            }
+
+        }
+
+    }
+
+    vector<int> dfsOfGraph(int V, vector<int> adj[])
+
+    {
+
+        // Code here
+
+        map<int, bool>visited;
+
+        vector<int>ans;
+
+        for (int i = 0; i < V; i++) {
+
+        if (visited[i] == 0)
+
+        dfs_helper(adj, ans, visited, i, -1);
+
+        }
+
+        return ans;
+
+    }
